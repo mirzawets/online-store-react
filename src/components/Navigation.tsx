0eab0ea026a54812126373bf580b7734fab385cf
mirzawets <93333417+mirzawets@users.noreply.github.com>
2023-12-21 15:21:21 +0400

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 export default function Navigation() {
   return (
     <nav className='h-[50px] flex justify-between items-center px-5 bg-gray-500 text-white'>
-      <span className='font-bold'>Online Store</span>
+      <span className='font-bold'>
+        <Link to='/online-store-react'>Online Store</Link>
+      </span>
       <span>
-        <Link to='/' className='mr-2'>
-          Products
+        <Link to='/contacts' className='mr-2'>
+          Contacts
         </Link>
         <Link to='/about'>About</Link>
       </span>

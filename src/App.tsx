9c9f2d2969ea13bd64ded95898c27549abef1330
mirzawets <93333417+mirzawets@users.noreply.github.com>
2023-12-21 14:40:10 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import { Route, Routes } from 'react-router-dom';
 import ProductsPage from './pages/ProductsPage';
-import ContactsPage from './pages/ContactsPage';
 import AboutPage from './pages/AboutPage';
 
 export default function App() {
@@ -11,7 +10,6 @@ export default function App() {
       <Navigation />
       <Routes>
         <Route path='/' element={<ProductsPage />} />
-        <Route path='/contacts' element={<ContactsPage />} />
         <Route path='/about' element={<AboutPage />} />
       </Routes>
     </>
